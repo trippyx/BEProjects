c75b36bcc46444ed99463b536cfb23a739e06fde
Kuldeep Singh <basican.kuldeep@gmail.com>
2022-10-15 17:26:14 +0530

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express()
-app.use(express.json)
+app.use(express.json())
 
 app.get('/hello',(req,res)=>{
     res.send({data : [{'book':'HelloWorld'}], success_s : 200, message: "Success"})
