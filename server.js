20c3e78a16d7ee0a2cc408f0827632efaf689e62
Kuldeep Singh <basican.kuldeep@gmail.com>
2022-10-15 17:32:20 +0530

    POST Same Api

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express()
 app.use(express.json())
 
 app.get('/hello',(req,res)=>{
+    console.log(req.query)
+    res.send({data : [{'book':'HelloWorld'}], success_s : 200, message: "Successs"})
+});
+app.post('/hello',(req,res)=>{
+    console.log(req.query)
     res.send({data : [{'book':'HelloWorld'}], success_s : 200, message: "Success"})
 });
-
 app.listen(process.env.PORT,()=>{
     console.log('Api hit recived')
     console.log('server started');
